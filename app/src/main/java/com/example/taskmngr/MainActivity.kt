6cd47773e5c572cb722ba44416f719cbf8027bce
FrancaTM <francatm@outlook.com>
2023-05-07 19:16:12 -0300

    finish layout add string resources

--- app/src/main/java/com/example/taskmngr/MainActivity.kt
+++ app/src/main/java/com/example/taskmngr/MainActivity.kt
@@ -3,13 +3,23 @@ package com.example.taskmngr
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.taskmngr.ui.theme.TaskMngrTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +32,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    TaskManagerApp()
                 }
             }
         }
@@ -30,17 +40,36 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun TaskManagerApp() {
+    AllCompletedScreen()
 }
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun AllCompletedScreen(modifier: Modifier = Modifier) {
+    val imagePainter = painterResource(R.drawable.ic_task_completed)
+
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(painter = imagePainter, contentDescription = null)
+        Text(
+            text = stringResource(R.string.all_tasks_completed),
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.padding(top = 24.dp, bottom = 8.dp)
+        )
+        Text(
+            text = stringResource(R.string.nice_work),
+            fontSize = 16.sp,
+            modifier = modifier
+        )
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun TaskManagerAppPreview() {
     TaskMngrTheme {
-        Greeting("Android")
+        TaskManagerApp()
     }
 }
